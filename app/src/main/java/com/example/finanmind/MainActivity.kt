13580cbc48db3b9ea63db793765eb776ba46b595
Jefferson Bueno Mobile Developer <jeffersonbuenoprofissional@gmail.com>
2023-09-05 15:20:25 -0300

    FEAT: add animation and add main header and refactor code

--- app/src/main/java/com/example/finanmind/MainActivity.kt
+++ app/src/main/java/com/example/finanmind/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.example.finanmind
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AttachMoney
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Money
 import androidx.compose.material.icons.filled.MoneyOff
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -36,13 +38,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finanmind.components.CardComponent
 import com.example.finanmind.components.HeaderCardComponent
+import com.example.finanmind.components.MainHeader
 import com.example.finanmind.models.Transaction
 import com.example.finanmind.models.Transactions
 import com.example.finanmind.viewmodel.home.HomeViewModel
@@ -70,61 +75,85 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel = viewModel()) {
     val transactions by homeViewModel.uiState.collectAsState()
-    val showOptions by remember {
+    var showOptions by rememberSaveable {
         mutableStateOf(false)
     }
-
+    val density = LocalDensity.current
 
     Scaffold(
         modifier = Modifier.padding(horizontal = 16.dp),
+        topBar = {
+            MainHeader(
+                invoices = transactions.invoicesTotal,
+                incomes = transactions.incomesTotal
+            )
+        },
         floatingActionButton = {
             Column(
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.End
             ) {
-                ExtendedFloatingActionButton(
-                    modifier = Modifier.height(50.dp),
-                    text = { Text("Receita") },
-                    icon = { Icon(Icons.Filled.AttachMoney, "Add incomes") },
-                    onClick = {
-                        homeViewModel.setIncomes(
-                            Transaction(
-                                value = 10.0,
-                                title = "Renda",
-                                dueDate = Date(),
-                                paid = false
-                            )
-                        )
+                AnimatedVisibility(
+                    visible = showOptions,
+                    enter = slideInVertically {
+                        with(density) { 120.dp.roundToPx() }
                     },
-                    elevation = FloatingActionButtonDefaults.elevation(8.dp)
-                )
-                ExtendedFloatingActionButton(
-                    modifier = Modifier.height(50.dp),
-                    text = { Text("Despesa") },
-                    icon = { Icon(imageVector = Icons.Filled.MoneyOff, contentDescription = "Add invoice") },
-                    onClick = {
-                        homeViewModel.setInvoices(
-                            Transaction(
-                                value = 10.0,
-                                title = "Débito",
-                                dueDate = Date(),
-                                paid = false
+                    exit = slideOutVertically {
+                        with(density) { 120.dp.roundToPx() }
+                    }
+                ) {
+                    ExtendedFloatingActionButton(
+                        modifier = Modifier.height(50.dp),
+                        text = { Text("Receita") },
+                        icon = { Icon(Icons.Filled.AttachMoney, "Add incomes") },
+                        onClick = {
+                            homeViewModel.setIncomes(
+                                Transaction(
+                                    value = 10.0,
+                                    title = "Renda",
+                                    dueDate = Date(),
+                                    paid = false
+                                )
                             )
-                        )
+                        },
+                        elevation = FloatingActionButtonDefaults.elevation(8.dp)
+                    )
+                }
+                AnimatedVisibility(
+                    visible = showOptions,
+                    enter = slideInVertically {
+                        with(density) { 60.dp.roundToPx() }
                     },
-                    elevation = FloatingActionButtonDefaults.elevation(8.dp)
-                )
+                    exit = slideOutVertically {
+                        with(density) { 60.dp.roundToPx() }
+                    }
+                ) {
+                    ExtendedFloatingActionButton(
+                        modifier = Modifier.height(50.dp),
+                        text = { Text("Despesa") },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Filled.MoneyOff,
+                                contentDescription = "Add invoice"
+                            )
+                        },
+                        onClick = {
+                            homeViewModel.setInvoices(
+                                Transaction(
+                                    value = 10.0,
+                                    title = "Débito",
+                                    dueDate = Date(),
+                                    paid = false
+                                )
+                            )
+                        },
+                        elevation = FloatingActionButtonDefaults.elevation(8.dp)
+                    )
+                }
                 FloatingActionButton(
                     modifier = Modifier,
                     onClick = {
-                        homeViewModel.setInvoices(
-                            Transaction(
-                                value = 10.0,
-                                title = "Débito",
-                                dueDate = Date(),
-                                paid = false
-                            )
-                        )
+                        showOptions = !showOptions
                     }
                 ) {
                     Icon(imageVector = Icons.Filled.Add, contentDescription = "add new transaction")
@@ -132,20 +161,28 @@ fun HomeScreen(homeViewModel: HomeViewModel = viewModel()) {
             }
         },
     ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            item { HeaderCardComponent("Receitas", transactions.incomesTotal) }
-            items(
-                items = transactions.incomes,
-                itemContent = { item -> CardComponent(item) }
-            )
-            item { HeaderCardComponent("Despesas", transactions.invoicesTotal) }
-            items(
-                items = transactions.invoices,
-                itemContent = { item -> CardComponent(item) }
-            )
-        }
+        ListComponent(innerPadding, transactions)
+    }
+}
+
+@Composable
+private fun ListComponent(
+    innerPadding: PaddingValues,
+    transactions: Transactions
+) {
+    LazyColumn(
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        item { HeaderCardComponent("Receitas", transactions.incomesTotal) }
+        items(
+            items = transactions.incomes,
+            itemContent = { item -> CardComponent(item) }
+        )
+        item { HeaderCardComponent("Despesas", transactions.invoicesTotal) }
+        items(
+            items = transactions.invoices,
+            itemContent = { item -> CardComponent(item) }
+        )
     }
 }
 
